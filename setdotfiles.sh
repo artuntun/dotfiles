38d8489de213efa837b94faa7c163f214f4fafcd
artuntun <arturo@uizrd.io>
2021-05-14 12:27:41 +0200

    added installation script

---
+++ setdotfiles.sh
@@ -0,0 +1,14 @@
+link () {
+  mkdir ~/.config
+  for file in $( ls -A ./config | grep -vE '\.exclude*|\.git$|\.gitignore|\.gitmodules|.*.md' ) ; do
+    # Silently ignore errors here because the files may already exist
+    ln -sv "$PWD/config/$file" "$HOME/.config" || true
+  done
+  for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|\.gitmodules|.*.md' ) ; do
+    # Silently ignore errors here because the files may already exist
+    echo $file
+    # ln -sv "$PWD/$file" "$HOME" || true
+  done
+}
+
+link
